7942b2447a843b32ec8783c6ed0f5a37f491c85f
Leonard <leonardbinet@gmail.com>
2016-11-07 19:14:54 +0100

    Suppression des jars, ajout d'un script shell de transfert de jar

--- SLAVE SHAVADOOP JAR/src/Main.java
+++ SLAVE SHAVADOOP JAR/src/Main.java
@@ -3,6 +3,8 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -12,15 +14,41 @@ public class Main {
     public static void main(String[] args) {
     	
     	// on prend en argument le nom d'un fichier
-		String dossier = "/cal/homes/lbinet/workspace/Sys_distribue/Sx/";
-		Path sx_input = Paths.get(dossier+args[0]);
+		String dossier = "/cal/homes/lbinet/workspace/Sys_distribue/";
+		//String dossier = "/Users/leonardbinet/Documents/Formation/Cours Telecom/T1_Systemes_repartis_IN727/Shavadoop/Sx/";
+		Path sx_input = Paths.get(dossier+"Sx/"+args[0]);
 		List<String> lignes;
+		
+		// path to write files
+		Path umx_output = Paths.get(dossier+"Umx/Um0");
+		
+		// try to read file
 		try {
 			lignes = Files.readAllLines(sx_input, Charset.forName("UTF-8"));
-		} catch (IOException e1) {
+			
+			// create dictionary unsorted map: maybe useful for later
+			HashMap<String, Integer> Umx = new HashMap<String, Integer>();
+			// create list to write
+			ArrayList<String> Umx_write = new ArrayList<String>();
+			// line after line
+			for (String ligne: lignes ){
+				// each line has words separeted by spaces
+				String[] words = ligne.split(" ");
+				for (String word: words){
+					// append to dictionary
+					Umx.put(word, 1);
+					// append to list
+					Umx_write.add(word + " 1");
+				}
+			}
+			Files.write(umx_output, Umx_write, Charset.forName("UTF-8"));	
+			} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		
+
+		
     }
 
 
---
+++ transfer_jars.sh
@@ -0,0 +1,2 @@
+
+scp -i ~/.ssh/telecom SLAVESHAVADOOP.jar MASTERSHAVADOOP.jar lbinet@ssh.enst.fr:/cal/homes/lbinet/workspace/Sys_distribue/ 
